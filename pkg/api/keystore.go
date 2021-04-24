d7dfc545ae25c34e295e63a61a95f973f6d7287e
c1982 <aspsrc@gmail.com>
2021-04-25 01:28:03 +0300

    feat: added keystore functions
    * ImportUser
    * ListUsers

--- pkg/api/keystore.go
+++ pkg/api/keystore.go
@@ -69,12 +69,46 @@ func (c Calls) ExportUser(username, password string) (user, encoding string, err
 	return user, encoding, err
 }
 
-//TODO: wait implementation
-func (c Calls) ImportUser(username, password, user, encoding string) (bool, error) {
-	return false, nil
+//ImportUser import a user. password must match the user’s password. username doesn’t have to match the username user had when it was exported.
+func (c Calls) ImportUser(username, password, user string) (bool, error) {
+	out := network.P{}
+	err := c.client.NewRequestFor(&out, "ext/keystore", "keystore.importUser",
+		network.P{
+			"username": username,
+			"password": password,
+			"user":     user,
+		})
+	if err != nil {
+		return false, err
+	}
+
+	if !out.Success() {
+		return false, errors.New("user could not be import")
+	}
+
+	return true, nil
 }
 
-//TODO: wait implementation
-func (c Calls) ListUsers(username, password, user, encoding string) (users []string, err error) {
+//ListUsers list the users in this keystore.
+func (c Calls) ListUsers() (users []string, err error) {
+	out := network.P{}
+	err = c.client.NewRequestFor(&out, "ext/keystore", "keystore.listUsers", nil)
+	if err != nil {
+		return users, err
+	}
+
+	values, ok := (out["users"]).([]interface{})
+	if !ok {
+		return users, errors.New("not valid type 'users'")
+	}
+
+	if len(values) < 1 {
+		return []string{}, nil
+	}
+
+	for _, v := range values {
+		users = append(users, v.(string))
+	}
+
 	return users, nil
 }
--- pkg/api/keystore_test.go
+++ pkg/api/keystore_test.go
@@ -27,3 +27,34 @@ func TestKeyStoreCreateUser(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestKeyStoreUserList(t *testing.T) {
+	ts := NewServer(`{
+		"jsonrpc":"2.0",
+		"id"     :1,
+		"result" :{
+			"users":[
+				"user1",
+				"user2",
+				"user3",
+				"user4"
+			]
+		}
+	}`)
+	defer ts.Close()
+
+	client, err := network.NewNodeClient(ts.URL, 1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	api := api.NewAPICalls(client)
+	users, err := api.ListUsers()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(users) != 4 {
+		t.Errorf("invalid user count. have: 4, got: %d", len(users))
+	}
+}
