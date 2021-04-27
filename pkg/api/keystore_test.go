b9d14a5f1a6139488ff61eea852ea0381e527fb1
c1982 <aspsrc@gmail.com>
2021-04-27 13:07:50 +0300

    chore: refactor unit test way

--- pkg/api/keystore_test.go
+++ pkg/api/keystore_test.go
@@ -3,86 +3,100 @@ package api_test
 import (
 	"avalanchego-client/pkg/api"
 	"avalanchego-client/pkg/network"
+	"fmt"
 	"testing"
 )
 
-func TestKeyStoreCreateUser(t *testing.T) {
-	ts := NewServer(`{
-		"jsonrpc":"2.0",
-		"id"     :1,
-		"result" :{
-			"success":false
-		}
-	}`)
-	defer ts.Close()
+func TestKeyStore(t *testing.T) {
+	calltests := []struct {
+		name     string
+		response string
+		fn       func(api.Calls) error
+	}{
+		{
+			name: "CreateUser",
+			response: `{
+				"jsonrpc":"2.0",
+				"id"     :1,
+				"result" :{
+					"success":false
+				}
+			}`,
+			fn: func(c api.Calls) error {
+				err := c.CreateUser("username", "password")
+				if err == nil {
+					return err
+				}
 
-	client, err := network.NewNodeClient(ts.URL, 1)
-	if err != nil {
-		t.Error(err)
+				return nil
+			},
+		}, {
+			name: "ListUsers",
+			response: `{
+				"jsonrpc":"2.0",
+				"id"     :1,
+				"result" :{
+					"users":[
+						"user1",
+						"user2",
+						"user3",
+						"user4"
+					]
+				}
+			}`,
+			fn: func(c api.Calls) error {
+				users, err := c.ListUsers()
+				if err != nil {
+					return err
+				}
+
+				if len(users) != 4 {
+					return fmt.Errorf("invalid user count. have: 4, got: %d", len(users))
+				}
+				return nil
+			},
+		}, {
+			name: "KeyStoreExportUser",
+			response: `{
+				"jsonrpc":"2.0",
+				"id"     :1,
+				"result" :{
+					"user":"4CsUh5sfVwz2jNrJXBVpoPtDsb4tZksWykqmxC5CXoDEERyhoRryq62jYTETYh53y13v7NzeReisi",
+					"encoding":"cb58"
+				}
+			}`,
+			fn: func(c api.Calls) error {
+				user, encoding, err := c.ExportUser("username", "password")
+				if err != nil {
+					return err
+				}
+				if user != "4CsUh5sfVwz2jNrJXBVpoPtDsb4tZksWykqmxC5CXoDEERyhoRryq62jYTETYh53y13v7NzeReisi" {
+					return fmt.Errorf("unexpected value :%s", user)
+				}
+
+				if encoding != "cb58" {
+					return fmt.Errorf("unexpected value :%s", encoding)
+				}
+				return nil
+			},
+		},
 	}
 
-	api := api.NewAPICalls(client)
-	err = api.CreateUser("username", "password")
-	if err == nil {
-		t.Error(err)
-	}
-}
+	for _, tt := range calltests {
+		_ = t.Run(tt.name, func(t *testing.T) {
+			server := NewServer(tt.response)
+			defer server.Close()
 
-func TestKeyStoreUserList(t *testing.T) {
-	ts := NewServer(`{
-		"jsonrpc":"2.0",
-		"id"     :1,
-		"result" :{
-			"users":[
-				"user1",
-				"user2",
-				"user3",
-				"user4"
-			]
-		}
-	}`)
-	defer ts.Close()
+			client, err := network.NewNodeClient(server.URL, 1)
+			if err != nil {
+				t.Error(err)
+			}
 
-	client, err := network.NewNodeClient(ts.URL, 1)
-	if err != nil {
-		t.Error(err)
-	}
-
-	api := api.NewAPICalls(client)
-	users, err := api.ListUsers()
-	if err != nil {
-		t.Error(err)
-	}
-
-	if len(users) != 4 {
-		t.Errorf("invalid user count. have: 4, got: %d", len(users))
-	}
-}
-
-func TestKeyStoreExportUser(t *testing.T) {
-	ts := NewServer(`{
-		"jsonrpc":"2.0",
-		"id"     :1,
-		"result" :{
-			"user":"4CsUh5sfVwz2jNrJXBVpoPtDsb4tZksWykqmxC5CXoDEERyhoRryq62jYTETYh53y13v7NzeReisi",
-			"encoding":"cb58"
-		}
-	}`)
-	defer ts.Close()
-
-	client, err := network.NewNodeClient(ts.URL, 1)
-	if err != nil {
-		t.Error(err)
-	}
-
-	api := api.NewAPICalls(client)
-	user, encoding, err := api.ExportUser("username", "password")
-
-	if user != "4CsUh5sfVwz2jNrJXBVpoPtDsb4tZksWykqmxC5CXoDEERyhoRryq62jYTETYh53y13v7NzeReisi" {
-		t.Errorf("unexpected value :%s", user)
-	}
-
-	if encoding != "cb58" {
-		t.Errorf("unexpected value :%s", encoding)
+			api := api.NewAPICalls(client)
+			err = tt.fn(api)
+			if err != nil {
+				t.Error(err)
+			}
+		})
 	}
 }
